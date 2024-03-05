25639ee61fb0c5dd7069e38fc91682d0db4ec68c
enzoshiotuqui <enzomshiotuqui@gmail.com>
2024-03-05 13:40:30 -0300

    feat: dotenv

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,13 +1,13 @@
+require("dotenv").config()
 const express = require("express")
 const path = require("path")
 const cors = require("cors")
 
-const port = 5000;
+const port = process.env.PORT
 
 const app = express()
 
 // config JSON and form data response
-
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
